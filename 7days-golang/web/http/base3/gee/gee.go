07d7666e70951502fa7741b57b35eb824fa20383
陈国太 <532439904@qq.com>
2022-08-02 15:37:06 +0800

    七天用Go从零实现系列 - [web框架-Gee Day1 Http 基础] - 优化

--- 7days-golang/web/http/base3/gee/gee.go
+++ 7days-golang/web/http/base3/gee/gee.go
@@ -5,14 +5,21 @@ import (
 	"net/http"
 )
 
+// HandlerFunc 路由匹配成功后执行的方法
 type HandlerFunc func(w http.ResponseWriter, r *http.Request)
 
+// KeySep key 连接符
+const KeySep = "_"
+
+// Engine 引擎
 type Engine struct {
 	router map[string]HandlerFunc
 }
 
+// ServeHTTP 实现 http.Handler interface 中的 http.Handler.ServeHTTP 方法
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
+	key := req.Method + KeySep + req.URL.Path
+
 	if handler, ok := engine.router[key]; ok {
 		handler(w, req)
 	} else {
@@ -20,23 +27,28 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+// New 初始化一个引擎
 func New() *Engine {
 	return &Engine{router: make(map[string]HandlerFunc)}
 }
 
+// addRoute 添加路由
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "_" + pattern
+	key := method + KeySep + pattern
 	engine.router[key] = handler
 }
 
+// GET 添加一个 get 请求路由
 func (engine *Engine) GET(pattern string, handler HandlerFunc) {
 	engine.addRoute("GET", pattern, handler)
 }
 
+// POST 添加一个 post 请求
 func (engine *Engine) POST(pattern string, handler HandlerFunc) {
 	engine.addRoute("POST", pattern, handler)
 }
 
+// Run 启动一个 http 服务
 func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
 }
--- 7days-golang/web/http/base3/main.go
+++ 7days-golang/web/http/base3/main.go
@@ -14,9 +14,9 @@ func main() {
 
 	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
 		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v) // %q 该值对应的单引号括起来的go语法字符字面值，必要时会采用安全的转义表示
 		}
 	})
 
-	r.Run(":9999")
+	r.Run(":9998")
 }
